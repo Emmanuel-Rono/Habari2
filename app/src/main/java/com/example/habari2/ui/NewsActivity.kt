a3ea8e48722b84bb5d9cbf1d3bd1d2773441c4f4
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2022-10-21 16:35:31 +0300

    Added Menu

--- app/src/main/java/com/example/habari2/ui/NewsActivity.kt
+++ app/src/main/java/com/example/habari2/ui/NewsActivity.kt
@@ -2,12 +2,25 @@ package com.example.habari2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.habari2.R
 
 class NewsActivity : AppCompatActivity() {
+    lateinit var navController:NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.NavHostContainer) as NavHostFragment
+        navController=navHostFragment.navController
+        setupActionBarWithNavController(navController)
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/habari2/ui/fragments/articleFragment.kt
+++ app/src/main/java/com/example/habari2/ui/fragments/articleFragment.kt
@@ -1,10 +1,6 @@
 package com.example.habari2.ui.fragments
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.habari2.R
 
 class articleFragment : Fragment(R.layout.fragment_article) {
