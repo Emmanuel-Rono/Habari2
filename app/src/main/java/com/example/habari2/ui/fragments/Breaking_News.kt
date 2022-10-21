dd448b384ccb062f1a665d7571db3c011320091e
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2022-10-21 11:03:25 +0300

    Ïnitial Commit

---
+++ app/src/main/java/com/example/habari2/ui/fragments/Breaking_News.kt
@@ -0,0 +1,9 @@
+package com.example.habari2.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.habari2.R
+
+class Breaking_News : Fragment(R.layout.fragment_breaking_news) {
+
+    }
+
