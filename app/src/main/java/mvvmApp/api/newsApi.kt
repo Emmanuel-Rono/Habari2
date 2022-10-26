0c6a03b059f646107a05f8402a8b0b23caacae65
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2022-10-26 21:59:03 +0300

    retrofit progress

--- app/src/main/java/mvvmApp/api/newsApi.kt
+++ app/src/main/java/mvvmApp/api/newsApi.kt
@@ -1,4 +1,39 @@
 package mvvmApp.api
 
-interface newsApi {
+import android.provider.Contacts.SettingsColumns.KEY
+import mvvmApp.NewsResponse
+import mvvmApp.ui.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface newsApi
+{
+    //To get all the breaing news from the API
+    @GET("v2/top-headlines")
+
+    suspend fun getBreakingnews(
+        @Query("country")
+        countrycode:String="us",
+        @Query("page")
+    pageNumber:Int =1,
+        @Query("apiKey")
+        apiKey:String=API_KEY
+    ):Response<NewsResponse>
+    //Above function should return A RESPONSE OF TYPE NewsResponse
+
+    //To get all the breaing news from the API
+    @GET("v2/everything")
+
+    suspend fun searchnews(
+        @Query("country")
+        countrycode:String="us",
+        @Query("page")
+        pageNumber:Int =1,
+        @Query("apiKey")
+        apiKey:String=API_KEY
+    ):Response<NewsResponse>
+    //Above function should return A RESPONSE OF TYPE NewsResponse
+
+
 }
---
+++ app/src/main/java/mvvmApp/ui/Constants.kt
@@ -0,0 +1,9 @@
+package mvvmApp.ui
+
+class Constants
+{
+    companion object {
+       const val API_KEY="651a832e7deb4cc6ab088d58e08d5d5d"
+    }
+
+}
