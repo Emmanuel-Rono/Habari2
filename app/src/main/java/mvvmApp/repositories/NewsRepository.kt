90aa865fefd08e1e7f2569bdc44534b2c9567906
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2022-11-08 16:22:33 +0300

    Changed some errors

--- app/src/main/java/mvvmApp/repositories/NewsRepository.kt
+++ app/src/main/java/mvvmApp/repositories/NewsRepository.kt
@@ -6,7 +6,6 @@ import mvvmApp.db.ArticleDatabase
 
 class NewsRepository(val database:ArticleDatabase)
  {
-
  suspend fun getBreakingNews(countrycode : String, pageNumber: Int )=
   RetrofitInstance.api.getBreakingnews(countrycode, pageNumber)
 }
--- app/src/main/java/mvvmApp/ui/newsViewModel.kt
+++ app/src/main/java/mvvmApp/ui/newsViewModel.kt
@@ -2,16 +2,33 @@ package mvvmApp.ui
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import mvvmApp.models.NewsResponse
 import mvvmApp.repositories.NewsRepository
 import mvvmApp.util.Resource
+import retrofit2.Response
 
 class newsViewModel( val newsRepository: NewsRepository
-): ViewModel()
-{
+): ViewModel() {
 
-    val breakingNews :MutableLiveData<Resource<NewsResponse>> =MutableLiveData()
-    val newsPage=1
-    fun getBreakingNews()
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val newsPage = 1
+    fun getBreakingNews(countrycode:String)=viewModelScope.launch {
+      breakingNews.postValue(Resource.Loading())
+        val response=newsRepository.getBreakingNews(countrycode,newsPage)
+
+    }
+private fun handleBreakingNews (response:Response<NewsResponse>): Resource.Success<NewsResponse>
+
+{
+if (response.isSuccessful)
+{
+    response.body()?.let { resultresponse ->
+        return Resource.Success(resultresponse)
+    }
+}
+    return Resource.Error(response.message())
+}
 
 }
